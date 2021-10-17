ddbce5902fb1a95d5ba72eafef3b273e49c70fa0
Shubham <shubhampandeyhaihum@gmail.com>
2021-10-17 12:19:07 +0530

    on & off toggle of video implemented with error of one time on off switch

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -1,31 +1,56 @@
 const video = document.querySelector('video');
+const audio = document.querySelector('audio');
+
 const mediaToggle = document.querySelectorAll('.stream__media-control');
+let vidsrc = video.src
 // const switchVideoLabel = document.querySelector('.stream__mediaControl-video label');
 // const switchVideoToggle = document.querySelector('.stream__mediaControl-video span');
 
 mediaToggle.forEach((media) => {
-    const mediaLabel = media.querySelector('label')
-    mediaLabel.addEventListener('click', function () {
-        media.querySelector('span').classList.toggle('active');
-        if (mediaLabel.getAttribute('for') == 'video-toggle') {
-            stream.getTracks().forEach(function (track) {
-                if (track.readyState == 'live' && track.kind === 'video') {
-                    track.enabled = false;
-                    console.log('stoped', track.readyState);
-                } else if (track.readyState == 'ended' && track.kind === 'video') {
-                    track.readyState = 'live'
-                    console.log('sytart', track.readyState, track);
-                    track.enabled = true;
-                }
-            });
-        }
+    // switch comps
+    let mediaLabel = media.querySelector('label')
+    let mediaLabelSpan = media.querySelector('span')
 
+    // switchLabel click event
+    mediaLabel.addEventListener('click', function () {
+        mediaLabelSpan.classList.toggle('active');
+        if (mediaLabel.getAttribute('for') == 'video-toggle') {
+
+            if (!mediaLabelSpan.classList.contains('active')) {
+                stream.getTracks().forEach(function (track) {
+                    if (track.kind === 'video') {
+                        console.log('stoped', track);
+                        track.stop();
+                        video.src = '';
+                    }
+                });
+            } else {
+                navigator.mediaDevices.getUserMedia({
+                        audio: true,
+                        video: true
+                    })
+                    .then(stream => {
+                        window.localStream = stream;
+                        video.srcObject = stream;
+                        audio.srcObject = stream;
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            }
+        }
     })
 })
 
-
 navigator.mediaDevices.getUserMedia({
-    video: true
-}).then((stream) => {
-    video.srcObject = stream;
-})
+        audio: true,
+        video: true
+    })
+    .then(stream => {
+        window.localStream = stream;
+        video.srcObject = stream;
+        audio.srcObject = stream;
+    })
+    .catch((err) => {
+        console.log(err);
+    });
