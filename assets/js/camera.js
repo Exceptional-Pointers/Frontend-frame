08b2988597c69f4255d38e370f1b5551b2adef11
Shubham <shubhampandeyhaihum@gmail.com>
2021-10-18 16:17:54 +0530

    Video toogle btn started working #### But #### audio toggle for mic on & off is not working. Audio track is getting off on mic toggle i.e. (track.enabled = false) changes the track object property enable to false but still mic continues to record our voice

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -2,7 +2,7 @@ const video = document.querySelector('video');
 const audio = document.querySelector('audio');
 
 const mediaToggle = document.querySelectorAll('.stream__media-control');
-let vidsrc = video.src
+let micTrack = ''
 // const switchVideoLabel = document.querySelector('.stream__mediaControl-video label');
 // const switchVideoToggle = document.querySelector('.stream__mediaControl-video span');
 
@@ -15,29 +15,30 @@ mediaToggle.forEach((media) => {
     mediaLabel.addEventListener('click', function () {
         mediaLabelSpan.classList.toggle('active');
         if (mediaLabel.getAttribute('for') == 'video-toggle') {
-
-            if (!mediaLabelSpan.classList.contains('active')) {
-                stream.getTracks().forEach(function (track) {
-                    if (track.kind === 'video') {
-                        console.log('stoped', track);
-                        track.stop();
-                        video.src = '';
+            stream.getTracks().forEach(function (track) {
+                if (track.kind === 'video') {
+                    if (!mediaLabelSpan.classList.contains('active')) {
+                        track.enabled = false;
+                        console.log('video stopped', track);
+                    } else {
+                        track.enabled = true;
+                        console.log('video started', track);
                     }
-                });
-            } else {
-                navigator.mediaDevices.getUserMedia({
-                        audio: true,
-                        video: true
-                    })
-                    .then(stream => {
-                        window.localStream = stream;
-                        video.srcObject = stream;
-                        audio.srcObject = stream;
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-            }
+                }
+            });
+        } else if (mediaLabel.getAttribute('for') == 'audio-toggle') {
+            stream.getTracks().forEach(function (track) {
+                if (track.kind === 'audio') {
+                    micTrack = track
+                    if (!mediaLabelSpan.classList.contains('active')) {
+                        track.enabled = false;
+                        console.log('audio stopped', track);
+                    } else {
+                        track.enabled = true;
+                        console.log('audio started', track);
+                    }
+                }
+            });
         }
     })
 })
